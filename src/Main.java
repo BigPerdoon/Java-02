5104bd2e5df572363449954a8e09ba6cae3f0c17
Crus <e_maverick@mail.ru>
2024-12-17 13:19:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+    public class Main {
+        public static void main(String[] args) {
+            // Объявляем переменные для входных данных
+            double ticketPrice = 13676.0; // стоимость билета в рублях
+            double rublesPerMile = 20.0;  // количество рублей для одной бонусной мили
+
+            // Рассчитываем количество бонусных миль
+            int bonusMiles = (int) (ticketPrice / rublesPerMile);
+
+            // Выводим результат на экран
+            System.out.println("Количество бонусных миль: " + bonusMiles);
+        }
+    }
